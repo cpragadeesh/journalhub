5614d39daa00da4a3286847fba0ec91c8f7784d2
Venkataraman T <venkatrt96@gmail.com>
2017-12-22 21:47:07 +0530

    fixed index error in recco and mongo

--- cloud.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import storage
-import os
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'journal-hub-50781-firebase-adminsdk-vc4qi-8ffbbce50a.json'
-
-client = storage.Client()
-bucket = client.bucket('journal-hub-50781.appspot.com')
-blob = bucket.blob('/')
-blob.download_as_string()
